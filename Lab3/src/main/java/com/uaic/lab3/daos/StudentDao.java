c2c7dc3bca43602d35105fd8aa5f719515cc1e21
DianaCrainic <dianacrainic801@yahoo.com>
2021-10-23 23:09:01 +0300

    create student functionality

--- Lab3/src/main/java/com/uaic/lab3/daos/StudentDao.java
+++ Lab3/src/main/java/com/uaic/lab3/daos/StudentDao.java
@@ -1,7 +1,10 @@
 package com.uaic.lab3.daos;
 
+import com.uaic.lab3.dtos.ExamDto;
+import com.uaic.lab3.dtos.StudentDto;
 import com.uaic.lab3.entities.Student;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -19,16 +22,26 @@ public class StudentDao extends Dao {
 
     public List<Student> getAll() throws SQLException {
         List<Student> students = new ArrayList<>();
-        String query = "SELECT id, name FROM students;";
+        String query = "SELECT id, name, exam_ids FROM students;";
         try (Statement statement = connection.createStatement()) {
             ResultSet resultSet = statement.executeQuery(query);
             while (resultSet.next()) {
                 Integer id = resultSet.getInt("id");
                 String name = resultSet.getString("name");
-                Student student = new Student(id, name, null);
+                String examIds = resultSet.getString("exam_ids");
+                Student student = new Student(id, name, examIds);
                 students.add(student);
             }
         }
         return students;
+    }
+
+    public void create(StudentDto studentDto) throws SQLException {
+        String command = "INSERT INTO students(name, exam_ids) VALUES (?, ?);";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(command)) {
+            preparedStatement.setString(1, studentDto.getName());
+            preparedStatement.setString(2, studentDto.getAssignedExams());
+            preparedStatement.execute();
+        }
     }
 }
