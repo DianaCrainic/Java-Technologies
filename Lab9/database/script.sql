a6a6158a34cdc90bcb526f5165412c43fc28f0c7
DianaCrainic <dianacrainic801@yahoo.com>
2022-01-09 20:21:28 +0200

    add health check and metrics options

--- Lab9/database/script.sql
+++ Lab9/database/script.sql
@@ -1,22 +1,22 @@
 create table if not exists users
 (
     username varchar(100) not null unique,
-    password varchar(50)  not null,
+    password varchar(64)  not null,
     primary key (username)
-);
+    );
 
 create table if not exists groups
 (
-    name varchar(50) not null unique,
+    name varchar(32) not null unique,
     primary key (name)
-);
+    );
 
 create table if not exists user_groups
 (
     username   varchar(100) not null references users (username) on delete cascade,
-    group_name varchar(10)  not null references groups (name) on delete restrict,
+    group_name varchar(32)  not null references groups (name) on delete restrict,
     primary key (username, group_name)
-);
+    );
 
 create table if not exists documents
 (
@@ -26,12 +26,12 @@ create table if not exists documents
     author_username     varchar(100) not null references users on delete restrict,
     content             bytea        not null,
     primary key (id)
-);
+    );
 
 insert into users(username, password)
-values ('admin', encode(sha256('admin'), 'hex'));
+values ('admin', encode(sha256('password'), 'hex'));
 insert into users(username, password)
-values ('author', encode(sha256('author'), 'hex'));
+values ('author', encode(sha256('password'), 'hex'));
 
 insert into groups(name)
 values ('admin');
@@ -42,8 +42,3 @@ insert into user_groups(username, group_name)
 values('admin', 'admin');
 insert into user_groups(username, group_name)
 values('author', 'author');
-
-insert into documents(registration_number, name, author_username, content)
-values (1234, 'Document1', 'author', 'Content of the first document');
-insert into documents(registration_number, name, author_username, content)
-values (45678, 'Document2', 'author', 'Content of the second document');
